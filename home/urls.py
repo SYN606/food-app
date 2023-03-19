e7ab906f244e518b5318515d6bedef3f756b9f42
SYN <postbox.501@yandex.com>
2023-03-20 00:41:44 +0530

    previous user creation method was not working well created new method and done changes in template

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='/'),
-    path('login', views.login, name='login'),
-    path('create_acc', views.create_account, name='create_acc')
+    path('login', views.user_login, name='login'),
+    path('create_acc', views.create_account, name='create_acc'),
+    path('logout', views.user_logout, name='logout')
 ]
--- home/views.py
+++ home/views.py
@@ -2,8 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
-from .models import *
 
 def index(request):
     return render(request, 'index.html')
@@ -11,25 +12,53 @@ def index(request):
 
 def create_account(request):
     if request.method == "POST":
-        name = request.POST['name']
+        username = request.POST['username']
+        first_name = request.POST['f_name']
+        last_name = request.POST['l_name']
         email = request.POST['email']
-        password = request.POST['password']
+        password_1 = request.POST['password-1']
+        password_2 = request.POST['password-2']
         phone_number = request.POST['phone_number']
 
-        new_user = Create_user(
-            name = name,
-            email = email,
-            password = password,
-            phone_number = phone_number
-        )
 
-        new_user.save()
+        if password_1 == password_2:
+            new_user = User.objects.create_user(username, email, password_1)
+            new_user.first_name = first_name
+            new_user.last_name = last_name
+            new_user.save()
 
-        messages.success(request, 'User Created Successfully')
+            messages.success(request, 'User Created Successfully')
+
+        else:
+            messages.error(request, 'password did not match please try again')
+
         return redirect('/')
     else:
         # messages.error("Something Went Wrong. Please Try Again.!!!")
         return render(request, 'create-acc.html')
 
-def login(request):
-    return render(request, 'login.html')
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST['name']
+        password = request.POST['password']
+
+        print(username, password)
+        user = authenticate(
+                            username = username, 
+                            password = password
+                            )
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully Logged In")
+            return redirect('/')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+    
+
+def user_logout(request):
+    logout(request)
+    return redirect('/')
