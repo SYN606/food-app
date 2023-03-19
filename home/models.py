9404ca77b4114f1785029cf86dc92c87ad94bb54
SYN <postbox.501@yandex.com>
2023-03-19 20:07:27 +0530

    created user creating in views.py and added it into template

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Create_user(models.Model):
+
+    name = models.CharField(max_length=25, null=False)
+    password = models.CharField(max_length=12, null=False)
+    email= models.EmailField(max_length=254)
+    phone_number = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,13 +1,35 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from django.contrib import messages
+
+from .models import *
 
 def index(request):
     return render(request, 'index.html')
 
 
+def create_account(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        phone_number = request.POST['phone_number']
+
+        new_user = Create_user(
+            name = name,
+            email = email,
+            password = password,
+            phone_number = phone_number
+        )
+
+        new_user.save()
+
+        messages.success(request, 'User Created Successfully')
+        return redirect('/')
+    else:
+        # messages.error("Something Went Wrong. Please Try Again.!!!")
+        return render(request, 'create-acc.html')
+
 def login(request):
     return render(request, 'login.html')
-
-def create_account(request):
-    return render(request, 'create-acc.html')
